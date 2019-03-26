bf46ab7375e376c1c2f735b5e9a9214764dea84a
Shalain DeRamus <shalainderamus@Shalains-MacBook-Pro.local>
2019-03-26 15:07:13 -0400

    finished clock

---
+++ 02 - JS and CSS Clock/clock.js
@@ -0,0 +1,22 @@
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand'); 
+const hourHand = document.querySelector('.hour-hand'); 
+
+const setDate = () => {
+    const now = new Date();
+
+    const seconds = now.getSeconds(); 
+    const secondsDegrees = ((seconds/60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`; 
+
+    const mins = now.getMinutes(); 
+    const minDegrees = ((mins/60) * 360) + 90; 
+    minHand.style.transform = `rotate(${minDegrees}deg)`; 
+
+    const hours = now.getHours(); 
+    const hourDegrees = ((hours/12) * 360) + 90; 
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`; 
+    console.log(seconds)
+}
+
+setInterval(setDate, 1000); 
